2560be23beca999340c8bb7c2db0b53b8ca3f2ec
Jessweb87 <jprosper87@gmail.com>
2022-02-17 21:06:51 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const projectRouter = require('./project/router')
 const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 const server = express()
 
@@ -10,6 +11,7 @@ server.use(express.json())
 
 server.use('/api/projects', projectRouter)
 server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
 
 server.use((err, req, res, next) => {
     res.status(500).json({message: err.message, stack: err.stack})
